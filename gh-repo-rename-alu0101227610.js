089b01213838fe7496ba475632dd5ea858a0c679
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-11-22 12:23:23 +0000

    añadidos los queries en bruto

--- gh-repo-rename-alu0101227610.js
+++ gh-repo-rename-alu0101227610.js
@@ -17,6 +17,24 @@ program.parse(process.argv);
 let args = program.args;
 debugger;
 
+const getRepoId = (owner, newName) => `
+query {
+  repository(owner: "${owner}", name: "${newName}") {
+    id
+  }
+}
+`;
+
+const renameRepo = (id) => `
+mutation {
+  updateRepository(input: {name: "prueba-funciona", repositoryId: ${id}}) {
+    repository {
+      name
+    }
+  }
+}
+`;
+
 let originalName = `${program.opts().name}`;
 
 let { org, repo, name } = program.opts();
@@ -27,8 +45,12 @@ if (!org || ! repo || !name) program.help();
 if (!shell.which('git')) shell.echo("git not installed")
 if (!shell.which('gh')) shell.echo("gh not installed");
 
+/*
 let r = shell.exec(`gh api -X PATCH /repos/${org}/${repo} -f name=${name}`, {silent: true});
 
 let rj = JSON.parse(r.stdout)
 console.log(`The repo ${org}/${repo} has been renamed to ${rj.full_name}`);
 //console.log(`The repo has been renamed to ${rj.full_name}`);
+*/
+
+console.log(getRepoId("ULL-MFP-AET-2122", "nuevonombredelrepo"))
